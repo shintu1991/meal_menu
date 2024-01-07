e8d6311620108caaaf44fa4bccd0e44856dd0be4
shintu1991 <116678986+shintu1991@users.noreply.github.com>
2024-01-07 09:33:46 -0800

    First commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import "../Footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h5 className="footer-text"> ©2024 The meal menu</h5>
+
+        </div>
+    );
+}
+
+export default Footer;
